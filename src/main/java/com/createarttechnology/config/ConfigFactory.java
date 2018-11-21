181889043bf4e83c424599569f841ff8c810ac44
XuhuiLee <lxh199108@gmail.com>
2018-11-21 11:51:19 +0800

    重复注册watcher

--- src/main/java/com/createarttechnology/config/ConfigFactory.java
+++ src/main/java/com/createarttechnology/config/ConfigFactory.java
@@ -1,15 +1,13 @@
 package com.createarttechnology.config;
 
+import com.createarttechnology.jutil.StringUtil;
 import com.createarttechnology.logger.Logger;
 import com.google.common.base.Preconditions;
-import com.createarttechnology.jutil.StringUtil;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.Watcher;
-
-import java.io.IOException;
 
 /**
  * Created by lixuhui on 2018/11/13.
@@ -20,12 +18,15 @@ public abstract class ConfigFactory {
 
     private static CuratorFramework client;
 
+    private static String profile = System.getProperty("process.properties.profile");
+
     public static Config load(String configName, ConfigWatcher configWatcher) {
         Preconditions.checkArgument(StringUtil.isNotEmpty(configName));
         Preconditions.checkNotNull(configWatcher);
 
         BaseConfig config = new BaseConfig();
         try {
+            // 初始化
             if (client == null) {
                 System.getProperties().load(ConfigFactory.class.getResourceAsStream("/process.properties"));
                 String zookeeperConnectionString = System.getProperty("process.properties.zookeeperConnectionString");
@@ -33,18 +34,13 @@ public abstract class ConfigFactory {
                 RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
                 client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
                 client.start();
-            }
-
-            loadConfig(config, configName, configWatcher);
-            client.getData().usingWatcher((Watcher) event -> {
-                switch (event.getType()) {
-                    case NodeDataChanged:
-                    case NodeDeleted:
-                        loadConfig(config, configName, configWatcher);
-                        return;
-                    default:
+                if (profile == null) {
+                    profile = "dev";
                 }
-            }).forPath(getConfigPath(configName));
+                logger.info("process.properties.profile:{}", profile);
+            }
+            // 先读一次
+            loadConfig(config, configName, configWatcher);
         } catch (Exception e) {
             logger.error("load config error, e:", e);
         }
@@ -57,19 +53,31 @@ public abstract class ConfigFactory {
             byte[] data = client.getData().forPath(getConfigPath(configName));
             if (data != null) {
                 config.load(new String(data));
+            } else {
+                config.load("");
             }
+            // 客户端watcher回调
             configWatcher.changed(config);
         } catch (Exception e) {
-            logger.error("error, e:", e);
+            logger.error("loadConfig error, e:", e);
+        }
+        // 每次都注册watcher
+        try {
+            client.getData().usingWatcher((CuratorWatcher) event -> {
+                switch (event.getType()) {
+                    case NodeDataChanged:
+                    case NodeDeleted:
+                        loadConfig(config, configName, configWatcher);
+                        return;
+                    default:
+                }
+            }).forPath(getConfigPath(configName));
+        } catch (Exception e) {
+            logger.error("getData error, e:", e);
         }
     }
 
     private static String getConfigPath(String configName) {
-        String profile = System.getProperty("process.properties.profile");
-        logger.info("process.properties.profile:{}", profile);
-        if (profile == null) {
-            profile = "dev";
-        }
         return "/config/" + profile + "/" + configName + "/" + configName;
     }
 
